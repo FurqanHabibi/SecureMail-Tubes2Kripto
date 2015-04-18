2326875e56a3e5cba43d737d498ca298c851db6e
alifanuraniputri <alifanuraniputri@gmail.com>
2015-04-18 13:13:03 +0700

    ECDSA with default curve

---
+++ src/algorithm/ECDSA.java
@@ -0,0 +1,225 @@
+package algorithm;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import algorithm.ECC;
+import algorithm.Point;
+
+public class ECDSA {
+
+	private String signature;
+	private String message;
+	private Point G; // generator point
+	private BigInteger n;
+	private BigInteger e; // e=H(m), H is hash function: SHA1
+	private BigInteger dA; // private key dA
+	private Point QA; // public key QA = dA * G
+	private BigInteger r; // r = x1 (mod n), where (x1, y1) = k * G. r !=0
+	private BigInteger s; // s = k-1 (e + dAr)(mod n), s != 0
+	private BigInteger w;
+	private BigInteger u1;
+	private BigInteger u2;
+	private BigInteger k;
+
+	public ECDSA() {
+
+	}
+
+	public BigInteger getN() {
+		return n;
+	}
+
+	public BigInteger getE() {
+		return e;
+	}
+
+	public void setE(BigInteger e) {
+		this.e = e;
+	}
+
+	public BigInteger getW() {
+		return w;
+	}
+
+	public void setW(BigInteger w) {
+		this.w = w;
+	}
+
+	public BigInteger getU1() {
+		return u1;
+	}
+
+	public void setU1(BigInteger u1) {
+		this.u1 = u1;
+	}
+
+	public BigInteger getU2() {
+		return u2;
+	}
+
+	public void setU2(BigInteger u2) {
+		this.u2 = u2;
+	}
+
+	public BigInteger getK() {
+		return k;
+	}
+
+	public void setK(BigInteger k) {
+		this.k = k;
+	}
+
+	public void setN(BigInteger n) {
+		this.n = n;
+	}
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Point getG() {
+		return G;
+	}
+
+	public void setG(Point g) {
+		G = g;
+	}
+
+	public BigInteger getdA() {
+		return dA;
+	}
+
+	public void setdA(BigInteger dA) {
+		this.dA = dA;
+	}
+
+	public Point getQA() {
+		return QA;
+	}
+
+	public void setQA(Point qA) {
+		QA = qA;
+	}
+
+	public BigInteger getR() {
+		return r;
+	}
+
+	public void setR(BigInteger r) {
+		this.r = r;
+	}
+
+	public BigInteger getS() {
+		return s;
+	}
+
+	public void setS(BigInteger s) {
+		this.s = s;
+	}
+
+	public void cal_e() {
+		SHA1 sha1 = new SHA1(message);
+		String hashed = sha1.hash();
+		e = new BigInteger(hashed, 16);
+	}
+
+	public static BigInteger generatePrivateKeyECDSA(BigInteger n) {
+		return new BigInteger(192, new Random())
+				.mod(n.subtract(BigInteger.ONE)).add(BigInteger.ONE);
+	}
+
+	public static Point generatePublicKeyECDSA(BigInteger dA, Point G) {
+		ECC.setParam(new BigInteger("1"),
+				new BigInteger("6"), new BigInteger("11"), new Point(new BigInteger(
+						"2"), new BigInteger("4")));
+		return ECC.times(dA, G);
+	}
+
+	public void signatureGeneration(BigInteger privateK, String _message,
+			BigInteger a, BigInteger b, BigInteger p, Point _G, BigInteger _n) {
+		
+		G = _G;
+		ECC.setParam(a, b, p, G);
+		dA = privateK;
+		message = _message;
+	
+		cal_e();
+		n = _n;
+		do {
+			k = new BigInteger(192, new Random()).mod(
+					n.subtract(BigInteger.ONE)).add(BigInteger.ONE);
+			Point kP = ECC.times(k, G);
+			r = kP.x.mod(n);
+			s = BigInteger.ONE.divide(k).multiply(e.add(dA.multiply(r))).mod(n);
+		} while (r.compareTo(BigInteger.ZERO) == 0
+				|| s.compareTo(BigInteger.ZERO) == 0);
+
+	}
+
+	public boolean verifySignature(Point publicK,  String _message,
+			BigInteger a, BigInteger b, BigInteger p, Point _G, BigInteger _n, BigInteger r, BigInteger s) {
+		boolean val = false;
+
+		ECC.setParam(a, b, p, G);
+		QA = publicK;
+		message=_message;
+
+		if (r.compareTo(n) != -1
+				|| r.compareTo(BigInteger.ZERO) != 1) {
+			return val;
+		}
+
+		if (s.compareTo(n) != -1
+				|| s.compareTo(BigInteger.ZERO) != 1) {
+			return val;
+		}
+
+		cal_e();
+		w = s.modInverse(n);
+		u1 = e.multiply(w).mod(n);
+		u2 = r.multiply(w).mod(n);
+		Point P = ECC.add(ECC.times(u1, G), ECC.times(u2, QA));
+		if (P.x.equals(r.mod(n)))
+			val = true;
+
+		return val;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BigInteger pri = ECDSA.generatePrivateKeyECDSA(new BigInteger("13"));
+		Point pub = ECDSA.generatePublicKeyECDSA(pri, new Point(new BigInteger(
+				"2"), new BigInteger("4")));
+		ECDSA ecdsa = new ECDSA();
+		
+		ecdsa.signatureGeneration(pri, "Alifa Nurani Putri syslalallalalala wkwk!?=0", new BigInteger("1"),
+				new BigInteger("6"), new BigInteger("11"), new Point(new BigInteger(
+						"2"), new BigInteger("4")), new BigInteger("13"));
+		
+		boolean valid = ecdsa.verifySignature(pub, "Alifa Nurani Putri syslalallalalala wkwk!?=0", new BigInteger("1"),
+				new BigInteger("6"), new BigInteger("11"), new Point(new BigInteger(
+						"2"), new BigInteger("4")), new BigInteger("13"), ecdsa.getR(), ecdsa.getS());
+		
+		if (valid)
+			System.out.println("benar");
+		else 
+			System.out.println("salah");
+
+	}
+
+}
--- src/algorithm/SHA1.java
+++ src/algorithm/SHA1.java
@@ -4,22 +4,21 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
 
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.Arrays;
 
 public class SHA1 {
 
-	String message;
-	byte[] byteMsg;
-	byte[] bytePadded;
-	String hexString;
-	int[] hblocks;
-	int[] word;
-	final int[] H = { 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476,
+	private String message;
+	private byte[] byteMsg;
+	private byte[] bytePadded;
+	private String hexString;
+	private int[] hblocks;
+	private int[] word;
+	private final int[] H = { 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476,
 			0xC3D2E1F0 };
-	final int[] K = { 0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6 };
+	private final int[] K = { 0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6 };
 
 	public SHA1() {
 
@@ -62,21 +61,26 @@ public class SHA1 {
 		this.hblocks = hblocks;
 	}
 
+	/**
+	 * Padding : add single 1 followed by 0s until 448 bit messages, then add 64
+	 * bit of messages size
+	 */
 	public void padding() {
 
 		byteMsg = new byte[message.length()];
-		for (int i=0; i<message.length(); i++) {
-			byteMsg[i] = (byte) ( (int) message.charAt(i) );
+		for (int i = 0; i < message.length(); i++) {
+			byteMsg[i] = (byte) ((int) message.charAt(i));
 		}
 		int sizeMsg = message.length() * 8;
 		if (sizeMsg % 512 != 448) {
-			byteMsg = Arrays.copyOf(byteMsg, byteMsg.length
-					+ (56 - (message.length() % 56)));
+			byteMsg = Arrays.copyOf(byteMsg,
+					byteMsg.length + (56 - (message.length() % 56)));
 			byteMsg[message.length()] = (byte) 128;
 		}
 
 		long size = Long.valueOf(sizeMsg);
-		byte[] sizeb = ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN).putLong(size).array();
+		byte[] sizeb = ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN)
+				.putLong(size).array();
 		bytePadded = new byte[byteMsg.length + sizeb.length];
 		System.arraycopy(byteMsg, 0, bytePadded, 0, byteMsg.length);
 		System.arraycopy(sizeb, 0, bytePadded, byteMsg.length, sizeb.length);
@@ -93,48 +97,6 @@ public class SHA1 {
 		return new byte[] { (byte) (value >>> 24), (byte) (value >>> 16),
 				(byte) (value >>> 8), (byte) value };
 	}
-
-	/**
-	 * 80 round hashing
-	 * */
-	public void hash() {
-		padding();
-		prepareW();
-		initializeHashBlocks();
-
-		for (int t = 0; t < 80; t++) {
-			
-			int f = SHAFunction(t, hblocks[0], hblocks[1], hblocks[2], hblocks[3], hblocks[4]);
-			int k = KFunction(t);
-			
-			int tmp =Integer.rotateLeft(hblocks[0], 5)+  f   + hblocks[4]   + k + word[t];
-			
-			hblocks[4] = hblocks[3];
-			hblocks[3] = hblocks[2];
-			hblocks[2] = Integer.rotateLeft(hblocks[1], 30);
-			hblocks[1] = hblocks[0];
-			hblocks[0] = tmp;
-			
-		}
-
-		hblocks[0] = hblocks[0] + H[0];
-		hblocks[1] = hblocks[1] + H[1];
-		hblocks[2] = hblocks[2] + H[2];
-		hblocks[3] = hblocks[3] + H[3];
-		hblocks[4] = hblocks[4] + H[4];
-		
-
-		hexString = "";
-		for (int i = 0; i < hblocks.length; i++) {
-			System.out.println("[hashed] : "
-					+ Integer.toHexString(hblocks[i]));
-			hexString = hexString + Integer.toHexString(hblocks[i]);
-		}
-		
-		System.out.println(hexString);
-
-	}
-
 
 	public int SHAFunction(int t, int a, int b, int c, int d, int e) {
 		int result = 0;
@@ -168,15 +130,64 @@ public class SHA1 {
 		return result;
 	}
 
+	/**
+	 * Prepare words : 80 element array. element 1-16 : 512 bit msg split into
+	 * 16 element 17-80 : word[i] = word[i - 16] ^ word[i - 14] ^ word[i - 8] ^
+	 * word[i - 3];
+	 * */
 	public void prepareW() {
 		word = new int[80];
 		for (int i = 0; i < 16; i++) {
-			word[i] = bytePadded[4*i] << 24 | (bytePadded[4*i+1] & 0xFF) << 16 | (bytePadded[4*i+2]& 0xFF) << 8 | (bytePadded[4*i+3] & 0xFF);
+			word[i] = bytePadded[4 * i] << 24
+					| (bytePadded[4 * i + 1] & 0xFF) << 16
+					| (bytePadded[4 * i + 2] & 0xFF) << 8
+					| (bytePadded[4 * i + 3] & 0xFF);
 		}
 		for (int i = 16; i < 80; i++) {
 			word[i] = word[i - 16] ^ word[i - 14] ^ word[i - 8] ^ word[i - 3];
 			word[i] = Integer.rotateLeft(word[i], 1);
 		}
+
+	}
+
+	/**
+	 * 80 round hashing
+	 * */
+	public String hash() {
+		padding();
+		prepareW();
+		initializeHashBlocks();
+
+		for (int t = 0; t < 80; t++) {
+
+			int f = SHAFunction(t, hblocks[0], hblocks[1], hblocks[2],
+					hblocks[3], hblocks[4]);
+			int k = KFunction(t);
+
+			int tmp = Integer.rotateLeft(hblocks[0], 5) + f + hblocks[4] + k
+					+ word[t];
+
+			hblocks[4] = hblocks[3];
+			hblocks[3] = hblocks[2];
+			hblocks[2] = Integer.rotateLeft(hblocks[1], 30);
+			hblocks[1] = hblocks[0];
+			hblocks[0] = tmp;
+
+		}
+
+		hblocks[0] = hblocks[0] + H[0];
+		hblocks[1] = hblocks[1] + H[1];
+		hblocks[2] = hblocks[2] + H[2];
+		hblocks[3] = hblocks[3] + H[3];
+		hblocks[4] = hblocks[4] + H[4];
+
+		hexString = "";
+		for (int i = 0; i < hblocks.length; i++) {
+			hexString = hexString + Integer.toHexString(hblocks[i]);
+		}
+
+		System.out.println(hexString);
+		return hexString;
 
 	}
 
